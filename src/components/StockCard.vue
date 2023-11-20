34431685913e6961e5496455785e199f2741e3d0
Nagendra <kavya.nagendra@harman.com>
2023-11-20 23:44:08 +0530

    fix conditional rendering

--- src/components/StockCard.vue
+++ src/components/StockCard.vue
@@ -7,47 +7,30 @@ const props = defineProps({
     required: true,
   }
 })
-const getBackgroundColor = () =>{
+const getStyles = () =>{
   if(props.stock?.tradeComparision==='high'){
-    return "bg-green-100";
+    return {
+      bgColor: "bg-green-100",
+      svgPath: "M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941",
+      textColor: "text-green-600",
+      strokeColor: "green"
+    };
   }
   else if(props.stock?.tradeComparision==='low'){
-    return "bg-red-100"
+    return {
+      bgColor: "bg-red-100",
+      svgPath: "M2.25 6L9 12.75l4.286-4.286a11.948 11.948 0 014.306 6.43l.776 2.898m0 0l3.182-5.511m-3.182 5.51l-5.511-3.181",
+      textColor: "text-red-600",
+      strokeColor: "red"
+    };
   }
   else{
-    return "bg-gray-100"
-  }
-  
-}
-const getSVGPath = () =>{
-  if(props.stock?.tradeComparision==='low'){
-    return "M2.25 6L9 12.75l4.286-4.286a11.948 11.948 0 014.306 6.43l.776 2.898m0 0l3.182-5.511m-3.182 5.51l-5.511-3.181"
-  }
-  else{
-    return "M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-  }
-  
-}
-const getTextColor = () => {
-  if(props.stock?.tradeComparision==='high'){
-    return "text-green-600"
-  }
-  else if(props.stock?.tradeComparision==='low'){
-    return "text-red-600"
-  }
-  else{
-    return "text-gray-500"
-  }
-}
-const getStrokeColor = () => {
-  if(props.stock?.tradeComparision==='high'){
-    return "green";
-  }
-  else if(props.stock?.tradeComparision==='low'){
-    return "red"
-  }
-  else{
-    return "gray"
+    return {
+      bgColor: "bg-gray-100",
+      svgPath: "M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941",
+      textColor: "text-gray-500",
+      strokeColor: "gray"
+    }
   }
 }
 </script>
@@ -62,12 +45,12 @@ const getStrokeColor = () => {
       </div>
 
       <!-- Right Column -->
-      <div :class="[getBackgroundColor(), 'flex ml-4 rounded text-left']">
+      <div :class="[getStyles().bgColor, 'flex ml-4 rounded text-left']">
           <div class="p-2 w-16">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" :stroke-width="1.5" :stroke="getStrokeColor()" class="w-4 h-4">
-              <path stroke-linecap="round" stroke-linejoin="round" :d="getSVGPath()" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" :stroke-width="1.5" :stroke="getStyles().strokeColor" class="w-4 h-4">
+              <path stroke-linecap="round" stroke-linejoin="round" :d="getStyles().svgPath" />
             </svg>
-            <span :class="getTextColor()">{{ stock?.peRatio }}%</span>
+            <span :class="getStyles().textColor">{{ stock?.peRatio }}%</span>
           </div>
       </div>
     </div>
